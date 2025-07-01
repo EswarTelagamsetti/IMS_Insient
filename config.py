aa83e779f97b83bf4dbe2790954f618b405ac211
[Eswar Telagamsetti] <[eswartelagamsetti1234@gmail.com]>
2025-07-01 21:03:00 +0530

    commit

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'insient-secret-key-2024'
     
     # MySQL Configuration
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
-    MYSQL_USER = os.environ.get('MYSQL_USER') or ' root'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ' your-password'
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'db_name'
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', '3306'))
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') 
+    MYSQL_USER = os.environ.get('MYSQL_USER') 
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_DB = os.environ.get('MYSQL_DB') 
     
     # Session Configuration
     PERMANENT_SESSION_LIFETIME = timedelta(hours=24)
@@ -17,5 +18,5 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your-email@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'your-email-password'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
